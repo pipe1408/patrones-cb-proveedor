71457e4224a2d2b7ad7c45f72fe29aa8b8acdea4
Felipe <pipeba1408@gmail.com>
2024-09-05 12:42:56 -0500

    Update Controller

--- src/main/java/com/felipe/patronescbproveedor/Controller.java
+++ src/main/java/com/felipe/patronescbproveedor/Controller.java
@@ -1,4 +1,20 @@
 package com.felipe.patronescbproveedor;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class Controller {
+
+    @GetMapping("/notificar")
+    public String notificar() {
+        return "Notificación enviada.";
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Pong";
+    }
 }
