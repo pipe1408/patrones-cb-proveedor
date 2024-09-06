f36e67f829a09a3ac299e831fec660a60829989c
pipeb <pipeba1408@gmail.com>
2024-09-06 01:02:14 -0500

    Update Controller
    *Agrega CrossOrigin
    *Elimina endpoint ping

--- src/main/java/com/felipe/patronescbproveedor/Controller.java
+++ src/main/java/com/felipe/patronescbproveedor/Controller.java
@@ -1,5 +1,6 @@
 package com.felipe.patronescbproveedor;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Controller {
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/notificar")
     public String notificar() {
         return ISOTimeGenerator.getTime();
-    }
-
-    @GetMapping("/ping")
-    public String ping() {
-        return "Pong";
     }
 }
